ddb9fc9430e314940c1b1b1f74e8a89edaff04cb
Blake Bishop <94153853+BalakayBishop@users.noreply.github.com>
2022-11-18 10:26:14 -0800

    file reformatting according to PyCharm recommendations

--- Company Blog/company_blog/blog_posts/forms.py
+++ Company Blog/company_blog/blog_posts/forms.py
@@ -3,8 +3,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class BlogPostForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	text = TextAreaField('Text', validators=[DataRequired()])
-	submit = SubmitField('Post')
-	
+    title = StringField('Title', validators=[DataRequired()])
+    text = TextAreaField('Text', validators=[DataRequired()])
+    submit = SubmitField('Post')
